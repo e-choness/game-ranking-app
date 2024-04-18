f588df4f444ff6a31b46a4bb47929af61d1a98b9
Echo Yin <echo_ybl@hotmail.com>
2024-04-18 00:03:12 -0400

    Add an event to handle the selected item

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["Skyrim", "BioShock", "Prey", "Deus Ex", "Ultima"];
+
+  const handleSelectedItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Games" />
+      <ListGroup
+        items={items}
+        heading="Games"
+        onSelectedItem={handleSelectedItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectedItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectedItem }: Props) {
   // Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
@@ -28,6 +29,7 @@ function ListGroup({ items, heading }: Props) {
             key={game}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectedItem(game);
             }}
           >
             {game}
