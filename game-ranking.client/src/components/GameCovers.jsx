95c0415f35c96ac36aca42fb52d5f9de2fe40b9c
Echo Yin <echo_ybl@hotmail.com>
2024-04-21 11:27:10 -0400

    Add game covers and query class

---
+++ game-ranking.client/src/components/GameCovers.jsx
@@ -0,0 +1,25 @@
+﻿import Skyrim from "@/assets/img-rpg/skyrim.jpg";
+import Oblivion from "@/assets/img-rpg/oblivion.jpg";
+import Morrowind from "@/assets/img-rpg/morrowind.jpg";
+import Enderal from "@/assets/img-rpg/enderal.jpg";
+import Gothic from "@/assets/img-rpg/gothic.jpg";
+import MassEffect from "@/assets/img-rpg/mass-effect.jpg";
+import DragonAge from "@/assets/img-rpg/dragon-age.jpg";
+import DivineDivinity from "@/assets/img-rpg/divine-divinity.jpg";
+import Diablo from "@/assets/img-rpg/diablo.jpg";
+import DiscoElysium from "@/assets/img-rpg/disco-elysium.jpg";
+
+const GameCovers = [
+    {id: 1, image: Skyrim},
+    {id: 3, image: Morrowind},
+    {id: 2, image: Oblivion},
+    {id: 4, image: Enderal},
+    {id: 5, image: Gothic},
+    {id: 6, image: MassEffect},
+    {id: 7, image: DragonAge},
+    {id: 8, image: DivineDivinity},
+    {id: 9, image: Diablo},
+    {id: 10, image: DiscoElysium}
+];
+
+export default GameCovers;
