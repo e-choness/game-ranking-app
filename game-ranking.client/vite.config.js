903aa1adfc36ca25b54eac5291fa2c6f9bfcff1e
Echo Yin <echo_ybl@hotmail.com>
2024-04-22 22:39:23 -0400

    Changed visual and rank title

--- game-ranking.client/vite.config.js
+++ game-ranking.client/vite.config.js
@@ -53,10 +53,6 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "^/weatherforecast": {
-        target,
-        secure: false,
-      },
       "^/item": {
         target,
         secure: false,
