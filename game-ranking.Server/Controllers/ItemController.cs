09539dc13b3e32b2f0b95c2b6d4e9a31c8332631
Echo Yin <echo_ybl@hotmail.com>
2024-04-22 23:30:34 -0400

    Update item info

--- game-ranking.Server/Controllers/ItemController.cs
+++ game-ranking.Server/Controllers/ItemController.cs
@@ -12,13 +12,13 @@ public class ItemController
         new ItemModel() { Id = 1, Title = "Skyrim", ImageId = 1, Ranking = 0, ItemType = 1 },
         new ItemModel() { Id = 2, Title = "Oblivion", ImageId = 2, Ranking = 0, ItemType = 1 },
         new ItemModel() { Id = 3, Title = "Morrowind", ImageId = 3, Ranking = 0, ItemType = 1 },
-        new ItemModel() { Id = 4, Title = "Prey", ImageId = 4, Ranking = 0, ItemType = 1 },
-        new ItemModel() { Id = 5, Title = "Deus Ex", ImageId = 5, Ranking = 0, ItemType = 1 },
-        new ItemModel() { Id = 6, Title = "Disco Elysium", ImageId = 6, Ranking = 0, ItemType = 1 },
-        new ItemModel() { Id = 7, Title = "Divine Divinity", ImageId = 7, Ranking = 0, ItemType = 1 },
-        new ItemModel() { Id = 8, Title = "Gothic", ImageId = 8, Ranking = 0, ItemType = 1 },
-        new ItemModel() { Id = 9, Title = "Mass Effect", ImageId = 9, Ranking = 0, ItemType = 1 },
-        new ItemModel() { Id = 10, Title = "Dragon Age", ImageId = 10, Ranking = 0, ItemType = 1 }
+        new ItemModel() { Id = 4, Title = "Enderal", ImageId = 4, Ranking = 0, ItemType = 1 },
+        new ItemModel() { Id = 5, Title = "Gothic", ImageId = 5, Ranking = 0, ItemType = 1 },
+        new ItemModel() { Id = 6, Title = "MassEffect", ImageId = 6, Ranking = 0, ItemType = 1 },
+        new ItemModel() { Id = 7, Title = "DragonAge", ImageId = 7, Ranking = 0, ItemType = 1 },
+        new ItemModel() { Id = 8, Title = "DivineDivinity", ImageId = 8, Ranking = 0, ItemType = 1 },
+        new ItemModel() { Id = 9, Title = "Diablo", ImageId = 9, Ranking = 0, ItemType = 1 },
+        new ItemModel() { Id = 10, Title = "DiscoElysium", ImageId = 10, Ranking = 0, ItemType = 1 }
     };
 
     [HttpGet("{type:int}")]
